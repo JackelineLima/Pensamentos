bc1a44b8e108f0e23cbc6f1b001d4e202bf0fa85
Jackeline Lima <jackelinelimap@gmail.com>
2022-08-18 17:27:24 -0300

    adicionando views no storyboard

---
+++ Pensamentos/Pensamentos/QuotesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QuotesViewController.swift
+//  Pensamentos
+//
+//  Created by Jackeline Pires De Lima on 08/08/22.
+//
+
+import UIKit
+
+class QuotesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Pensamentos/Pensamentos/SettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.swift
+//  Pensamentos
+//
+//  Created by Jackeline Pires De Lima on 08/08/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
